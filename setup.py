125f2a6ba97883a91352781cb4135af2d018b1f5
zach <zachkdavis00@gmail.com>
2023-03-25 20:35:03 -0400

    get ready for pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 setup(name='Plotter',
 version='0.1',
 description='Testing installation of Package',
-url='#',
-author='auth',
-author_email='',
+# url='#',
+author='Zachary Davis',
+author_email='zachkdavis00@gmail.com',
 license='MIT',
 packages=['Plotter'],
+install_requires=[
+        "matplotlib>=3.7.1",
+        "numpy==1.24.2"
+        ],
 zip_safe=False)
